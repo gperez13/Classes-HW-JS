b14a0f13f975cba123040a4cd08078dcec3e1cae
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-20 17:21:55 -0500

    Commit 4 - Created Person Class

--- classesHw.js
+++ classesHw.js
@@ -82,17 +82,66 @@ sprinkles.getPrice();
 
 
 
+//number 6
+
+class Person {
+	constructor(name){
+		this.name = name;
+		this.age = 0;
+		this.weight = 0;
+		this.mood = 0;
+		this.pets = [];
+		this.bankAccount = 100;
+	}
+	getName(){
+		return this.name
+	}
+	getAge(){
+		return this.age
+	}
+	getWeight(){
+		return this.weight
+	}
+	greet(otherPerson){
+		alert('Hi ' + otherPerson + ' how are you?!')
+	}
+	eat(){
+		this.weight += 5;
+		this.mood += 100;
+	}
+	exercise(){
+		this.weight -= 1
+	}
+	aging(){
+		this.age += 1;
+		this.weight +=3;
+		this.mood -=20;
+		this.bankAccount +=10;
+
+	}
+	buyPet(animalName){
+		this.pets.push(animalName);
+		let cost = animalName.getPrice();
+		return this.bankAccount - cost;
+
+	}
+
+}
 
 
+//number 7
 
+const jill = new Person('Jill');
+jill.getName();
+jill.getAge();
+jill.getWeight();
+jill.greet('Gio');
+jill.eat();
+jill.exercise();
+jill.aging();
+jill.buyPet(sparky);
 
-
-
-
-
-
-
-
+jill //<----to double check
 
 
 
