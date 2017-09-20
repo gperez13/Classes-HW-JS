35f7b0291311498a4c216398e2c947c18f91ac4a
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-20 16:41:47 -0500

    Commit 2 - Created Dog Class

--- classesHw.js
+++ classesHw.js
@@ -22,11 +22,29 @@ bobbyThePug.setOwner('Gio')
 //number 2
 
 
+class Dog extends Pet {
+	constructor(name, price){
+		super(name);
+		this.price = price;
+	}
+	bark(){
+		console.log('bark')
+	}
+	chaseTail(){
+		console.log('oh boy oh boy oh boy')
+	}
+	getPrice(){
+		return this.price
+	}
+}
 
 
 
+const sparky = new Dog('Sparky', 15.99);
 
-
+sparky.bark();
+sparky.chaseTail();
+sparky.getPrice();
 
 
 
