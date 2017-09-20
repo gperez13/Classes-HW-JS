02a28e66f476b85ea043a404fbbdf6f2b6231b9a
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2017-09-20 16:29:04 -0500

    Commit 1- Created Pet Class

---
+++ classesHw.js
@@ -0,0 +1,61 @@
+//number 1
+
+class Pet {
+	constructor(name){
+		this.name = name;
+		this.owner = '';
+	}
+	setOwner(dud){
+		this.owner = dud;
+		console.log(this)
+	}
+}
+
+const bobbyThePug = new Pet('Bobby');
+
+bobbyThePug.setOwner('Gio')
+
+
+
+
+
+//number 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
